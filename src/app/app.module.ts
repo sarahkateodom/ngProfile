0bf593cfe789866910f62908efd52211db996478
Sarah Odom <sarahkateodom@gmail.com>
2018-02-02 13:52:55 -0600

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SliderComponent
+    SliderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
