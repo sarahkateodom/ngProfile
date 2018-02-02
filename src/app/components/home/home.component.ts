0d75e7c62fb800e8927cc849cf152281b23dc564
Sarah Odom <sarahkateodom@gmail.com>
2018-02-02 11:59:08 -0600

    new theme

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { WOW } from 'wowjs/dist/wow.min';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  title = 'app';
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    new WOW().init();
+  }
+
+  constructor() {
+  }
 }
